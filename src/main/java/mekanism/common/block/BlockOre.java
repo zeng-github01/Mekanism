a55da83826814a250134da327fa3f29f82b57ef1
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2025-10-20 23:40:36 +0800

    Update the version number
     Fluorite Ore now drops fluorite and experience. [close #143]

--- src/main/java/mekanism/common/block/BlockOre.java
+++ src/main/java/mekanism/common/block/BlockOre.java
@@ -1,6 +1,7 @@
 package mekanism.common.block;
 
 import mekanism.common.Mekanism;
+import mekanism.common.MekanismItems;
 import mekanism.common.block.states.BlockStateOre;
 import mekanism.common.block.states.BlockStateOre.EnumOreType;
 import net.minecraft.block.Block;
@@ -8,10 +9,17 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 
 /**
  * Block class for handling multiple ore block IDs. 0: Osmium Ore 1: Copper Ore 2: Tin Ore
@@ -56,4 +64,46 @@ public class BlockOre extends Block {
             list.add(new ItemStack(this, 1, ore.ordinal()));
         }
     }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        if (state.getValue(BlockStateOre.typeProperty).ordinal() == EnumOreType.FLUORITE.ordinal()) {
+            return MekanismItems.FluoriteClump;
+        } else {
+            return super.getItemDropped(state, rand, fortune);
+        }
+    }
+
+    @Override
+    public int getExpDrop(IBlockState state, IBlockAccess world, BlockPos pos, int fortune) {
+        if (getItemDropped(state, RANDOM, fortune) == MekanismItems.FluoriteClump) {
+            return MathHelper.getInt(RANDOM, 1, 4);
+        } else {
+            return super.getExpDrop(state, world, pos, fortune);
+        }
+    }
+
+
+    @Override
+    public int quantityDropped(IBlockState state, int fortune, Random random) {
+        if (getItemDropped(state, RANDOM, fortune) == MekanismItems.FluoriteClump) {
+            return MathHelper.getInt(random, 2, 4) + random.nextInt(fortune + 1);
+        } else {
+            return super.quantityDropped(state, fortune, random);
+        }
+    }
+
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        Random rand = world instanceof World isWorld ? isWorld.rand : RANDOM;
+        int count = this.quantityDropped(state, fortune, rand);
+        for (int i = 0; i < count; i++) {
+            Item item = this.getItemDropped(state, rand, fortune);
+            if (item != Items.AIR) {
+                drops.add(new ItemStack(item, 1, item == MekanismItems.FluoriteClump ? 0 : this.damageDropped(state)));
+            }
+        }
+    }
+
+
 }
