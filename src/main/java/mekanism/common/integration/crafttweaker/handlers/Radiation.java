5ab764409eca71b4a2ae3080acfd79004b679c33
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2026-02-13 23:03:55 +0800

    更新添加辐射机制

--- src/main/java/mekanism/common/integration/crafttweaker/handlers/Radiation.java
+++ src/main/java/mekanism/common/integration/crafttweaker/handlers/Radiation.java
@@ -2,11 +2,11 @@ package mekanism.common.integration.crafttweaker.handlers;
 
 import crafttweaker.annotations.ZenRegister;
 import mekanism.api.Coord4D;
+import mekanism.api.MekanismAPI;
 import mekanism.common.Mekanism;
 import mekanism.common.integration.crafttweaker.gas.IGasStack;
 import mekanism.common.integration.crafttweaker.helpers.GasHelper;
 import mekanism.common.integration.crafttweaker.helpers.IngredientHelper;
-import mekanism.common.lib.radiation.RadiationManager;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -19,8 +19,13 @@ public class Radiation {
     @ZenMethod
     public static void addRadiation(IGasStack gasInput, double posX, double posY, double posZ, int dimensionId) {
         if (IngredientHelper.checkNotNull(NAME, gasInput)) {
-            RadiationManager.INSTANCE.dumpRadiation(new Coord4D(posX, posY, posZ, dimensionId), GasHelper.toGas(gasInput));
+            MekanismAPI.getRadiationManager().dumpRadiation(new Coord4D(posX, posY, posZ, dimensionId), GasHelper.toGas(gasInput));
         }
+    }
+
+    @ZenMethod
+    public static void addRadiation(double magnitude, double posX, double posY, double posZ, int dimensionId) {
+        MekanismAPI.getRadiationManager().radiate(new Coord4D(posX, posY, posZ, dimensionId), magnitude);
     }
 
 }
