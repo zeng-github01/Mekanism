d48b2476a1036dd8641385c866fabe7477e9432a
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2026-02-20 23:47:16 +0800

    更新背景

--- src/main/java/mekanism/generators/common/MekanismGenerators.java
+++ src/main/java/mekanism/generators/common/MekanismGenerators.java
@@ -50,7 +50,7 @@ import net.minecraftforge.oredict.OreDictionary;
         @Mod.CustomProperty(k = "license", v = "EUPL-1.2"),
         @Mod.CustomProperty(k = "issueTrackerUrl", v = "https://github.com/sddsd2332/Mekanism-CE-Unofficial-1.12.2/issues"),
         @Mod.CustomProperty(k = "iconFile", v = "assets/mekanism/icon.png"),
-        @Mod.CustomProperty(k = "backgroundFile", v = "assets/mekanism/background.png")
+        @Mod.CustomProperty(k = "backgroundFile", v = "assets/mekanismgenerators/background.png")
 })
 @Mod.EventBusSubscriber()
 public class MekanismGenerators implements IModule {
--- src/main/java/mekanism/multiblockmachine/common/MekanismMultiblockMachine.java
+++ src/main/java/mekanism/multiblockmachine/common/MekanismMultiblockMachine.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
         @Mod.CustomProperty(k = "license", v = "EUPL-1.2"),
         @Mod.CustomProperty(k = "issueTrackerUrl", v = "https://github.com/sddsd2332/Mekanism-CE-Unofficial-1.12.2/issues"),
         @Mod.CustomProperty(k = "iconFile", v = "assets/mekanism/icon.png"),
-        @Mod.CustomProperty(k = "backgroundFile", v = "assets/mekanism/background.png")
+        @Mod.CustomProperty(k = "backgroundFile", v = "assets/mekanismmultiblockmachine/background.png")
 })
 @Mod.EventBusSubscriber()
 public class MekanismMultiblockMachine implements IModule {
