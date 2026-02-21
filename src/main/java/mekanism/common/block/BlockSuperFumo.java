fb30ac5d02eb22fc3f792d6e2a6de04cc1ff001c
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2026-02-21 19:05:18 +0800

    Allow the super fumo to rotate

--- src/main/java/mekanism/common/block/BlockSuperFumo.java
+++ src/main/java/mekanism/common/block/BlockSuperFumo.java
@@ -5,13 +5,16 @@ import mekanism.common.Mekanism;
 import mekanism.common.integration.wrenches.Wrenches;
 import mekanism.common.util.MekanismUtils;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
@@ -22,11 +25,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockSuperFumo extends Block {
 
+
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
+
     public BlockSuperFumo() {
         super(Material.IRON);
         setHardness(50F);
         setResistance(2000F);
         setCreativeTab(Mekanism.tabMekanism);
+        setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
 
@@ -46,6 +53,9 @@ public class BlockSuperFumo extends Block {
                         MekanismUtils.dismantleBlock(this, state, world, pos);
                         return true;
                     }
+                    world.setBlockState(pos, state.withProperty(FACING, state.getValue(FACING).rotateYCCW()), 3);
+                    world.notifyNeighborsOfStateChange(pos, this, true);
+                    return true;
                 }
             }
         }
@@ -79,4 +89,47 @@ public class BlockSuperFumo extends Block {
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.CUTOUT;
     }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumfacing = EnumFacing.byIndex(meta);
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
+            enumfacing = EnumFacing.NORTH;
+        }
+        return this.getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getHorizontalIndex();
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing,
+                                            float hitX, float hitY, float hitZ, int meta,
+                                            EntityLivingBase placer, EnumHand hand) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+        return state.withProperty(FACING, mirrorIn.mirror(state.getValue(FACING)));
+    }
+
+
 }
